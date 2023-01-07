15af1fda96fc5501687bb98691ff80c6c2e58f6b
k15r <git@riddance.k15r.xyz>
2023-01-07 20:23:44 +0100

    Setup git.

--- index.ts
+++ index.ts
@@ -1,4 +1,6 @@
+import { init } from './lib/git.js'
 import { makePackageJson } from './lib/package.js'
 
 const cwd = process.cwd()
+await init(cwd)
 await makePackageJson(cwd)
---
+++ lib/git.ts
@@ -0,0 +1,32 @@
+import { mkdir, writeFile } from 'node:fs/promises'
+import { basename, join } from 'node:path'
+import { vote } from './siblings.js'
+
+export async function init(path: string) {
+    await mkdir(join(path, '.git/objects'), { recursive: true })
+    await mkdir(join(path, '.git/refs'), { recursive: true })
+    await writeFile(join(path, '.git/HEAD'), 'ref: refs/heads/main\n')
+    const config = await vote(
+        path,
+        '.git/config',
+        content =>
+            '[' +
+            content
+                .split('[')
+                .filter(section => section.startsWith('remote "origin"]'))
+                .join('[')
+                .replace(/(url\s*=.*\/)([^/]+)(\.git\n)/u, '$1%$3'),
+    )
+    await writeFile(
+        join(path, '.git/config'),
+        `[core]
+	repositoryformatversion = 0
+	filemode = false
+	bare = false
+	logallrefupdates = true
+${config?.replace('/%.git', `/${basename(path)}.git`) ?? ''}[branch "main"]
+	remote = origin
+	merge = refs/heads/main
+`,
+    )
+}
