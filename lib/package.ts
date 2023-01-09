6410492c142f6a64180efba65403eae7c7dc4348
k15r <git@riddance.k15r.xyz>
2023-01-09 01:06:31 +0100

    Reduce json indentation.

--- lib/package.ts
+++ lib/package.ts
@@ -40,7 +40,7 @@ export async function makePackageJson(path: string) {
                 ...packageJson,
             },
             undefined,
-            '    ',
+            '  ',
         ),
     )
 }
