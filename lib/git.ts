41aef08be95719fa173bc379b7553bc71216fc52
k15r <git@riddance.k15r.xyz>
2024-09-10 19:18:12 +0200

    Include git user.

--- lib/git.ts
+++ lib/git.ts
@@ -7,11 +7,15 @@ export async function init(path: string) {
     await mkdir(join(path, '.git/refs'), { recursive: true })
     await writeFile(join(path, '.git/HEAD'), 'ref: refs/heads/main\n')
     const config = await vote(path, '.git/config', content => {
-        const remotes = content.split('[').filter(section => section.startsWith('remote "origin"]'))
-        if (remotes.length === 0) {
+        const remotesAndUser = content
+            .split('[')
+            .filter(
+                section => section.startsWith('remote "origin"]') || section.startsWith('user]'),
+            )
+        if (remotesAndUser.length === 0) {
             return
         }
-        return '[' + remotes.join('[').replace(/(url\s*=.*\/)([^/]+)(\.git\n)/u, '$1%$3')
+        return '[' + remotesAndUser.join('[').replace(/(url\s*=.*\/)([^/]+)(\.git\n)/u, '$1%$3')
     })
     await writeFile(
         join(path, '.git/config'),
